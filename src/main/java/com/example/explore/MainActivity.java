fb5e689f174d8dc7c1794275afb5edf8327713ad
Chaitrali Shinde <56486246+Chaitrali-Shinde@users.noreply.github.com>
2020-05-23 19:10:52 +0530

    Add files via upload

--- src/main/java/com/example/explore/MainActivity.java
+++ src/main/java/com/example/explore/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_street, btn_market;
+    Button btn_street, btn_market, btn_pinkFood, btn_pinkMarket, btn_delhiFood, btn_DelhiMarket;
 
 
     @Override
@@ -19,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
         btn_market= (Button)findViewById(R.id.market);
         btn_street= (Button)findViewById(R.id.street);
+
+        btn_pinkMarket= (Button)findViewById(R.id.pinkmarket);
+        btn_pinkFood= (Button)findViewById(R.id.pinkfood);
+
+        btn_DelhiMarket= (Button)findViewById(R.id.delhimarket);
+        btn_delhiFood= (Button)findViewById(R.id.delhifood);
 
         btn_market.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,9 +36,39 @@ public class MainActivity extends AppCompatActivity {
         btn_street.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openStreetActivity();
+                openFoodActivity();
             }
         });
+
+        btn_pinkMarket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPinkMarketActivity();
+            }
+        });
+
+        btn_pinkFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPinkFoodActivity();
+            }
+        });
+
+        btn_DelhiMarket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDelhiMarketActivity();
+            }
+        });
+
+        btn_delhiFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDelhiFoodActivity();
+            }
+        });
+
+
     }
 
     public  void openMarketActivity(){
@@ -41,9 +77,34 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public  void  openStreetActivity(){
+    public  void  openFoodActivity(){
 
         Intent intend= new Intent(this, street.class);
+        startActivity(intend);
+    }
+
+    public  void openPinkMarketActivity(){
+        Intent intend= new Intent(this, pinkstreet.class);
+        startActivity(intend);
+
+    }
+
+    public  void  openPinkFoodActivity() {
+
+        Intent intend = new Intent(this, pinkfood.class);
+        startActivity(intend);
+
+    }
+
+    public  void openDelhiMarketActivity(){
+        Intent intend= new Intent(this, delhistreet.class);
+        startActivity(intend);
+
+    }
+
+    public  void  openDelhiFoodActivity(){
+
+        Intent intend= new Intent(this, delhifood.class);
         startActivity(intend);
     }
 }
